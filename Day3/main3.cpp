f16451745be893636987de04b297e6feedcba376
Lasermorph <stefanashansen@hotmail.com>
2022-03-24 23:02:50 +0100

    input file

--- Day3/main3.cpp
+++ Day3/main3.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+std::string getGamma(std::string s)
+{
+    return "";
+}
 
 int main()
 {
+    std::ifstream input;
+    input.open("input.txt");
 
+    if(input.is_open())
+    {
+        std::string s = "";
+        while(getline(input, s))
+        {
+            std::cout << s << std::endl;
+        }
+    }
 }
